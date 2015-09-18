ec2cb27459312582b3f0673cbc1448424afa6a8c
daniyuu <daniyuu@163.com>
2015-09-18 10:47:30 +0800

    2 4 6

---
+++ trymybest/Try_My_Best/Try_My_Best/008.String_To_Interger.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string str) {
+        cout << "test" << endl;
+        return 0;
+    }
+};
+
+int main(){
+    cout<< "here" << endl;
+}
