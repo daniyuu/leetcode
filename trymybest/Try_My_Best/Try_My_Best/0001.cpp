90c994eaec3330a1b92c5da07033f02c4632f35e
daniyuu <daniyuu@163.com>
2015-09-29 15:09:46 +0800

    something

---
+++ trymybest/Try_My_Best/Try_My_Best/0001.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+
+
+//
+//int main(){
+//    int n;
+//    cin >> n;
+//    
+//    map<string,int> m;
+////    string init = "abcdefghijkl";
+//    string init = "123";
+//    string nxt = init;
+//    int index = 1;
+//    m[init] = 1;
+//    next_permutation(nxt.begin(), nxt.end());
+////    cout << nxt << endl;
+//    while (nxt != init) {
+//        m[nxt] = index;
+////        cout << "hhh   " << nxt << "  " << index;
+//        next_permutation(nxt.begin(), nxt.end());
+//        ++index;
+//    }
+//    
+//    
+//    while (n--) {
+//        string s;
+//        cin >> s;
+//        cout << m[s] << endl;
+//    }
+//
+//}
+
+
+//struct so{
+//    int x,y;
+//} o[2] = {1,2,3,4};
+//
+//int main(){
+//    so *p = o[2];
+//    ++p->x;
+//    ++p->y;
+//}
+
+//
+//#include<stdio.h>
+//char *myString()
+//{
+//    char buffer[6] = {0};
+//    char *s = "Hello World!";
+//    for (int i = 0; i < sizeof(buffer) - 1; i++)
+//    {
+//        buffer[i] = *(s + i);
+//    }
+//    return buffer;
+//}
+//int main(int argc, char **argv)
+//{
+//    printf("%s\n", myString());
+//    return 0;
+//}
