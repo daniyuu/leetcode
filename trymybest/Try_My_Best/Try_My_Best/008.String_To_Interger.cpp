9158f6f93bac5478e64489f0dab2fd1e5d6a2d59
daniyuu <daniyuu@163.com>
2015-10-03 11:09:18 +0800

    008 010

--- trymybest/Try_My_Best/Try_My_Best/008.String_To_Interger.cpp
+++ trymybest/Try_My_Best/Try_My_Best/008.String_To_Interger.cpp
@@ -4,8 +4,35 @@ using namespace std;
 class Solution {
 public:
     int myAtoi(string str) {
-        cout << "test" << endl;
-        return 0;
+        int num = 0;
+        int sign = 1;
+        int n = str.length();
+        int i = 0;
+        
+        while (str[i] == ' ' && i < n) {
+            i++;
+        }
+        
+        if (str[i] == '-') {
+            sign = -1;
+            ++i;
+        }else if(str[i] == '+'){
+            ++i;
+        }
+        
+        for (; i < n; ++i) {
+            if (str[i] < '0' || str[i] > '9') {
+                break;
+            }
+            
+            if (num > INT_MAX / 10 || (num == INT_MAX / 10 && (str[i] - '0') > INT_MAX % 10 )) {
+                return sign == -1? INT_MIN: INT_MAX;
+            }
+            
+            num = num * 10 + str[i] - '0';
+        }
+        
+        return num * sign;
     }
 };
 
