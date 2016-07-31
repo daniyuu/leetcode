f8ceccada0797e7301463940e22df340ec596758
Daniyuu <daniyuu@163.com>
2016-07-31 16:25:41 +0800

    set new solution for projects

---
+++ sol/sol/Add_Two_Numbers/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Add Two numbers" << endl;
+	system("pause");
+}
---
+++ sol/sol/Two_Sum/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Two Sum" << endl;
+	system("pause");
+}
